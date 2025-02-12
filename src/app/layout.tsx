28c960e31a677510101dee897a7cf56a9e1e2d06
dennisDev77 <diniscamela5@gmail.com>
2025-02-12 19:36:19 +0100

    Update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import "./globals.css";
 
-export const metadata = {
-  title: "Lembrei+",
-  description: "Gestor de tarfas",
-};
+  export const metadata = {
+    title: "Home | Lembrei +",
+    description: "Gestor de tarfas",
+  };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-pt">
+    <html lang="pt-br">
       <body>
         {children}
       </body>
