10e823b1a32287485e43716bc0bc0c7617b9494b
dennisDev77 <diniscamela5@gmail.com>
2025-02-13 19:53:46 +0100

    Update Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,14 @@ export default function Home() {
 
   return ( 
     <>
-    <section className="container  flex flex-col flex-wrap justify-center items-center h-lvh">
+    
+    <main className="container  flex flex-col flex-wrap justify-start items-center h-lvh">
 
-      <div className={styles.boarder_user}>
+      <div className={`${styles.boarder_user} pt-10`}>
         <img src="/images/board-user.svg" alt="Carregando board"/>
       </div>
 
-      <div className='text-center font-medium text-3xl'>
+      <div className='text-center font-medium text-2xl'>
         <h2>Uma ferramenta para o seu dia a dia Escreva, Planeje e Organize - se </h2>
         <p className='font-normal text-xl'><span className='text-color_destaque1'>100% Gratuito</span> e Online</p>
       </div>
@@ -26,7 +27,7 @@ export default function Home() {
         </div>
 
       </div>
-    </section>
+    </main>
     </>
   );
 }
