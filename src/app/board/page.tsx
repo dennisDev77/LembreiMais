8cf7e1c671c3df2dfa8b0afb6284e72aeac79daf
dennisDev77 <diniscamela5@gmail.com>
2025-02-13 18:38:56 +0100

    Update to Board

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -10,29 +10,33 @@ const page = () => {
         
         <section className={`${styles.formContent}  p-4 rounded-md bg-color_black  `}>
 
-            <form className={`flex justify-center items-center gap-1 `}>
+            <form className={`flex justify-center items-center gap-1 pb-8`}>
                 <div className='w-full'>
                 <Input placeholder='Dgite a sua tarefa!' type='text'/>
                 </div>
 
                 <div className='w-16'>
-                <Button><FiPlus/></Button>
+                <Button> <span className='text-lg'><FiPlus/></span></Button>
                 </div>
             </form>
 
-            <article className={styles.taskList}>
-            <p>Aprenda a dizer Nao as Empresas</p>
+            <article className={`${styles.taskList} text-color_white p-2`}>
+            <p className='text-xl text-color_destaque1'>Aprenda a dizer Nao as Empresas</p>
 
-            <div className={styles.action}>
-                <div className={styles.date}>
-                <span ></span>
+            <div className={'flex justify-between items-center flex-wrap '}>
+
+                <div className={`flex justify-center items-center gap-1`}>
                 <span><FiCalendar/></span>
+                <time >14 Fev 2026</time>
                 </div>
-
-                <div className={styles.delete}>
+              
+                <div>
+                <button className={`flex justify-center items-center gap-1`}>
                 <span ><FiTrash/></span>
                 <span>Excluir</span>
+                </button>
                 </div>
+
             </div>
         </article>
             
