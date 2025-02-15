d3aebda389d7af5b521f8063e51104e1d76d3026
dennisDev77 <diniscamela5@gmail.com>
2025-02-15 23:33:44 +0100

    Remove picture 1 to Donate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,14 +14,13 @@ export default function Home() {
 
       <div className='text-center font-medium text-2xl'>
         <h2>Uma ferramenta para o seu dia a dia Escreva, Planeje e Organize - se </h2>
-        <p className='font-normal text-xl'><span className='text-color_destaque1'>100% Gratuito</span> e Online</p>
+        <p className='font-normal text-xl'><span className='font-medium'>100% Gratuito</span> e Online</p>
       </div>
 
       <div className={`${styles.apoiadores} text-center`}>
         <h2 className='font-normal text-xl'>Apoiadores</h2>
 
         <div className='flex justify-center items-center gap-2 pt-2'>
-          <img src="/images/profile1.png" alt="Carregando Apoiadores" className='h-14 w-14'/>
           <img src="/images/profile1.png" alt="Carregando Apoiadores" className='h-14 w-14'/>
           <img src="/images/profile1.png" alt="Carregando Apoiadores" className='h-14 w-14'/>
         </div>
