221ba4859dd929f45c6bdc820fc97568c6d379f0
dennisDev77 <diniscamela5@gmail.com>
2025-02-16 00:03:55 +0100

    add message and button Donate

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -2,47 +2,74 @@ import React from 'react'
 import Input from '../../components/Forms/Input/Input'
 import styles from './page.module.scss'
 import Button from '../../components/Forms/Button/Button'
-import {FiPlus, FiCalendar, FiTrash} from 'react-icons/fi'
+import {FiPlus, FiCalendar, FiTrash, FiEdit3, FiClock} from 'react-icons/fi'
+import SupportButton from '../../components/SupportButton/SupportButton'
 
 const page = () => {
   return (
+    <>
     <main className='container'> 
-        
-        <section className={`${styles.formContent}  p-4 rounded-md bg-color_black  `}>
+        {/* Add Tarefa */}
+        <section className={`${styles.formContent}  p-4 rounded-md shadow-md `}>
 
-            <form className={`flex justify-center items-center gap-1 pb-8`}>
-                <div className='w-full'>
-                <Input placeholder='Dgite a sua tarefa!' type='text'/>
-                </div>
+            {/* Form add tarefa */}
+          <form className={`flex justify-center items-center gap-1 pb-6`}>
+              <div className='w-full'>
+              <Input placeholder='Dgite a sua tarefa!' type='text'/>
+              </div>
 
-                <div className='w-16'>
-                <Button> <span className='text-lg'><FiPlus/></span></Button>
-                </div>
-            </form>
-
-            <article className={`${styles.taskList} text-color_white p-2`}>
-            <p className='text-xl text-color_destaque1'>Aprenda a dizer Nao as Empresas</p>
-
-            <div className={'flex justify-between items-center flex-wrap '}>
-
-                <div className={`flex justify-center items-center gap-1`}>
-                <span><FiCalendar/></span>
-                <time >14 Fev 2026</time>
-                </div>
-              
-                <div>
-                <button className={`flex justify-center items-center gap-1`}>
-                <span ><FiTrash/></span>
-                <span>Excluir</span>
-                </button>
-                </div>
-
-            </div>
-        </article>
+              <div className='w-16'>
+              <Button> <span className='text-lg'><FiPlus/></span></Button>
+              </div>
+          </form>
             
+          <h3 className='text-xl'>Voce tem 2 Tarefas</h3>
+        {/* Show Task */}
+          <article className={`${styles.taskList} text-color_white bg-color_black rounded-sm p-4`}>
+              <p className='text-base cursor-pointer pb-2'>Aprenda a dizer Nao as Empresas </p>
+
+              <div className={'flex justify-between items-center flex-wrap '}>
+
+                <div className={`flex justify-center items-center gap-4`}>
+                    <div className='flex justify-center items-center gap-1'>
+                    <span className='text-color_destaque1'><FiCalendar/></span>
+                    <time >14 Fev 2026</time>
+                    </div>
+
+                    <button className='flex justify-center items-center gap-1 w-20'>
+                      <span className='text-color_destaque1'> <FiEdit3/> </span>
+                      <span>Editar</span>
+                    </button>
+                </div>
+                
+                <div>
+                  <button className={`flex justify-center items-center gap-1`}>
+                  <span  className='text-red-500'><FiTrash/></span>
+                  <span>Excluir</span>
+                  </button>
+                </div>
+
+              </div> 
+          </article>
+
         </section >
 
+        {/* Mensagem do apoiador */}
+        <section className={`${styles.apoioVip} bg-color_black rounded-sm text-color_white p-4`}>
+          <h3 className='text-color_destaque1 text-xl'>Obrigado Por apoiar nosso Projecto</h3>    
+
+          <div className='mt-2 flex justify-start items-center gap-1'>
+            <span className='text-color_destaque1'><FiClock/></span>
+            <time>Ultima Doacao foi a 3 dias</time>    
+          </div>
+        </section>
     </main>
+
+  {/* Botao Apoiador */}
+    <section className=''>
+      <SupportButton/>
+    </section>
+    </>
   )
 }
 
