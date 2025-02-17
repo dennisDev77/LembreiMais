14f939471864d3929767333abd3a09d72512d3e7
dennisDev77 <diniscamela5@gmail.com>
2025-02-17 14:16:56 +0100

    Update authenticate

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -7,14 +7,42 @@ export const authOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      authorization:{params:{scope:'read:user user:email'}}
+      authorization:{params:{scope:'read:user'}}
     }),
-    
   ],
 
+ 
+  // callbacks:{
+  //   async session({session, token}){
+  //     try{
+  //      return{
+  //       ...session,
+  //       id:token.sub
+  //      }
+  //     }catch{
+  //       return{
+  //         ...session,
+  //         id:null
+  //       }
+  //     }
+  //   },
 
-  secret: process.env.NEXTAUTH_SECRET,
-}
+  //   async signIn({user, account, token}){
+  //     const {email}=user
+
+  //     try{
+  //       return true
+  //     }catch(err){
+  //       console.log(err)
+  //       return false
+  //     }
+  //   },
+  // }
+ 
+  // //Our secret
+  // secret:process.env.NEXTAUTH_SECRET
+  
+  }
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,44 +9,42 @@ import { FiX, FiGithub } from 'react-icons/fi';
 import {signIn, signOut, useSession} from 'next-auth/react'
 
 
-const Header = () => {
+export default function Header(){
     const {data:session}=useSession()
 
   return (
     <div className=' shadow-sm bg-color_black sticky top-0 left-0  z-50 text-color_white'>
-            <header className={`${styles.header} container flex justify-between items-center flex-wrap md:px-0 px-4`}>
+        <header className={`${styles.header} container flex justify-between items-center flex-wrap md:px-0 px-4`}>
 
-                <div className={styles.logo}>
-                  <Link href='/'> <img src='/logo.png' alt="Carregando logo" className='h-16' /></Link>
-                </div>
+            <div className={styles.logo}>
+              <Link href='/'> <img src='/logo.png' alt="Carregando logo" className='h-16' /></Link>
+            </div>
 
-                <nav className={`${styles.menu} font-medium text-lg flex justify-center items-center gap-4`}>
-                  <Link href='/'> Home </Link>
-                  <Link href='/board'> Meu Board  </Link>
-                </nav>
+            <nav className={`${styles.menu} font-medium text-lg flex justify-center items-center gap-4`}>
+              <Link href='/'> Home </Link>
+              <Link href='/board'> Meu Board  </Link>
+            </nav>
 
-                {
-                  session ?
-                (<div className={styles.entrar}>
-                    <SignInButton type='button' className='font-medium flex justify-center items-center gap-1' onClick={()=>signOut()}>
-                      <img src={session.user.image} alt="" className='h-10 w-10' />
-                      <span>Ola {session.user.name.split(' ')[0]}</span>
-                      <span className='text-xl text-color-black'><FiX/></span>
-                    </SignInButton>
-                </div>)
-                  :
-                (<div className={styles.entrar}>
-                    <SignInButton type='button' className='font-medium flex justify-center items-center gap-1' onClick={()=>signIn('github')}>
-                      <span className='text-3xl text-color_destaque1'><FiGithub/></span>
-                      <span>Entrar com github</span>
-                    </SignInButton>
-                </div>)
+            {
+              session ?
+            (<div className={styles.entrar}>
+                <SignInButton type='button' className='font-medium flex justify-center items-center gap-1' onClick={()=>signOut()}>
+                  <img src={session.user.image} alt="" className='h-10 w-10' />
+                  <span>Ola {session.user.name.split(' ')[0]}</span>
+                  <span className='text-xl text-color-black'><FiX/></span>
+                </SignInButton>
+            </div>)
+              :
+            (<div className={styles.entrar}>
+                <SignInButton type='button' className='font-medium flex justify-center items-center gap-1' onClick={()=>signIn('github')}>
+                  <span className='text-3xl text-color_destaque1'><FiGithub/></span>
+                  <span>Entrar com github</span>
+                </SignInButton>
+            </div>)
 
-                }
+            }
 
-            </header>
+        </header>
     </div>
   )
 }
-
-export default Header
